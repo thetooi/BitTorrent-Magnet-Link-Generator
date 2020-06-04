f199f0a24cee5856b9ebf5509aa6aabef5cda7a6
Phisarn Budwong <o-hite@hotmail.com>
2020-06-04 21:12:21 +0700

    Add project files.

---
+++ BitTorrent Magnet Link Generator/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace BitTorrent_Magnet_Link_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtINFO_HASH = new System.Windows.Forms.TextBox();
+            this.txtTORRENT_NAME = new System.Windows.Forms.TextBox();
+            this.txtMAGNET_LINK = new System.Windows.Forms.TextBox();
+            this.txtTRACKER_LIST = new System.Windows.Forms.TextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.linkMAGNET = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtINFO_HASH
+            // 
+            this.txtINFO_HASH.Location = new System.Drawing.Point(101, 18);
+            this.txtINFO_HASH.MaxLength = 40;
+            this.txtINFO_HASH.Name = "txtINFO_HASH";
+            this.txtINFO_HASH.Size = new System.Drawing.Size(366, 20);
+            this.txtINFO_HASH.TabIndex = 0;
+            this.txtINFO_HASH.TextChanged += new System.EventHandler(this.txtINFO_HASH_TextChanged);
+            // 
+            // txtTORRENT_NAME
+            // 
+            this.txtTORRENT_NAME.Location = new System.Drawing.Point(101, 72);
+            this.txtTORRENT_NAME.Name = "txtTORRENT_NAME";
+            this.txtTORRENT_NAME.Size = new System.Drawing.Size(366, 20);
+            this.txtTORRENT_NAME.TabIndex = 1;
+            this.txtTORRENT_NAME.TextChanged += new System.EventHandler(this.txtTORRENT_NAME_TextChanged);
+            // 
+            // txtMAGNET_LINK
+            // 
+            this.txtMAGNET_LINK.Location = new System.Drawing.Point(101, 158);
+            this.txtMAGNET_LINK.Name = "txtMAGNET_LINK";
+            this.txtMAGNET_LINK.Size = new System.Drawing.Size(366, 20);
+            this.txtMAGNET_LINK.TabIndex = 2;
+            this.txtMAGNET_LINK.Click += new System.EventHandler(this.txtMAGNET_LINK_Click);
+            // 
+            // txtTRACKER_LIST
+            // 
+            this.txtTRACKER_LIST.Location = new System.Drawing.Point(15, 19);
+            this.txtTRACKER_LIST.Multiline = true;
+            this.txtTRACKER_LIST.Name = "txtTRACKER_LIST";
+            this.txtTRACKER_LIST.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTRACKER_LIST.Size = new System.Drawing.Size(282, 305);
+            this.txtTRACKER_LIST.TabIndex = 3;
+            this.txtTRACKER_LIST.Text = resources.GetString("txtTRACKER_LIST.Text");
+            this.txtTRACKER_LIST.TextChanged += new System.EventHandler(this.txtTRACKER_LIST_TextChanged);
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(101, 120);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 4;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "info hash:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "torrent name:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTRACKER_LIST);
+            this.groupBox1.Location = new System.Drawing.Point(473, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(315, 340);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Trackers to include into the magnet link:";
+            // 
+            // linkMAGNET
+            // 
+            this.linkMAGNET.AutoSize = true;
+            this.linkMAGNET.Location = new System.Drawing.Point(26, 161);
+            this.linkMAGNET.Name = "linkMAGNET";
+            this.linkMAGNET.Size = new System.Drawing.Size(69, 13);
+            this.linkMAGNET.TabIndex = 9;
+            this.linkMAGNET.TabStop = true;
+            this.linkMAGNET.Text = "Magnet Link:";
+            this.linkMAGNET.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkMAGNET_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(310, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Example: A94A8FE5CCB19BA61C4C0873D391E987982FBBD3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(280, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "(this is not an URL, this is a title for the torrent in the client)";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(182, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(101, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Copy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 212);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGen;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(800, 364);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkMAGNET);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.txtMAGNET_LINK);
+            this.Controls.Add(this.txtTORRENT_NAME);
+            this.Controls.Add(this.txtINFO_HASH);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BitTorrent Magnet Link Generator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtINFO_HASH;
+        private System.Windows.Forms.TextBox txtTORRENT_NAME;
+        private System.Windows.Forms.TextBox txtMAGNET_LINK;
+        private System.Windows.Forms.TextBox txtTRACKER_LIST;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel linkMAGNET;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ BitTorrent Magnet Link Generator/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitTorrent_Magnet_Link_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtINFO_HASH_TextChanged(object sender, EventArgs e)
+        {
+            txtINFO_HASH.CharacterCasing = CharacterCasing.Upper;
+            GenLink();
+        }
+
+        private void txtTORRENT_NAME_TextChanged(object sender, EventArgs e)
+        {
+            GenLink();
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            GenLink();
+        }
+
+        private void txtTRACKER_LIST_TextChanged(object sender, EventArgs e)
+        {
+            GenLink();
+        }
+
+        private void GenLink()
+        {
+            if (txtINFO_HASH.Text != "" && txtINFO_HASH.Text.Length == 40)
+            {
+                string strtext = txtTRACKER_LIST.Text;
+                var textArr = strtext.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                int txtLine = textArr.Length;
+                string TORRENT_NAME;
+                try
+                {
+                    if (txtTORRENT_NAME.Text == "")
+                    {
+                        TORRENT_NAME = txtINFO_HASH.Text;
+                    }
+                    else
+                    {
+                        TORRENT_NAME = txtTORRENT_NAME.Text;
+                    }
+                    var trackers = txtTRACKER_LIST.Text;
+                    var data = "magnet:"
+                        + "?xt=urn:btih:" + txtINFO_HASH.Text
+                        + "&dn=" + Uri.EscapeDataString(TORRENT_NAME);
+                    for (int i = 0; i <= txtLine - 1; i++)
+                    {
+                        data += "&tr=" + Uri.EscapeDataString(txtTRACKER_LIST.Lines[i]);
+                    }
+                    txtMAGNET_LINK.Text = data;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+            }
+        }
+
+        private void linkMAGNET_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (txtMAGNET_LINK.Text != "")
+            {
+                System.Diagnostics.Process.Start(txtMAGNET_LINK.Text);
+            }
+        }
+
+        private void txtMAGNET_LINK_Click(object sender, EventArgs e)
+        {
+            txtMAGNET_LINK.SelectAll();
+            txtMAGNET_LINK.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtINFO_HASH.Clear();
+            txtTORRENT_NAME.Clear();
+            txtMAGNET_LINK.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtMAGNET_LINK.Text);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("bitcoin:bc1qmrvjhkc6kc79uw049nuquls4pknex3la03fyk4?label=Donate to BitTorrent Magnet Link Generator");
+        }
+    }
+}
